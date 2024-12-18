c3034a215fc7217d9b614b007058de773986369f
Iamtokenyes <92936554+Iamtokenyes@users.noreply.github.com>
2024-12-18 14:32:04 +0800

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -10,6 +10,8 @@ make clean
 cp ../config/${TARGETARCH}-config .config
 make -j16
 export PATH=`pwd`/output/host/bin:$PATH
+tar -zcvf ${TARGETARCH}-linux-buildroot.tar.gz output/host/*
+cp ${TARGETARCH}-linux-buildroot.tar.gz /releases/
 cd ..
 
 
